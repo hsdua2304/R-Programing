346d743c8efff4fd7327a66d453336b41656fcc4
hsdua2304 <hsdua2304@hotmail.com>
2017-09-17 12:13:23 +0530

    Data Manipulation and Preparation

--- Codes/DataManipulation.R
+++ Codes/DataManipulation.R
@@ -44,7 +44,10 @@ q2<-subset(stores,TotalSales>100,select=-c(1:10))
 q3<subset(stores,TotalSales>100,select=c('AcqCostPercust','BasketSize'))
 View(q1)
 
+# Droping variables
 
+d1<-stores[!names(stores)%in%c('Location')]
+View(d1)
 
 # CREATING NEW VARIABLES
 
@@ -58,6 +61,9 @@ View(stores)
 stores<-transform(stores, TotalCost1=AcqCostPercust*Total_Customers + OperatingCost)
 View(stores)
 
+require(plyr)
+data1<-mutate(stores,S=TotalCost+OperatingCost)
+View(data1)
 
 #BINNING VARAIBLES
 
@@ -119,4 +125,148 @@ duplicates<-stores[duplicated(stores$StoreName) & duplicated(stores$AcqCostPercu
 View(duplicates)
 
 
+# JOINING DATASETS OR MERGING
 
+dg<-read.csv('E:/R-Programing/Datasets/Demographic_Data.csv',header = T)
+ts<-read.csv('E:/R-Programing/Datasets/Transaction_Summary.csv',header = T)
+
+View(head(dg))
+View(head(ts))
+
+# Inner Join
+
+dg_ts_inner<-merge(dg,ts, by.x = c('CustName'),by.y = c('CustomerName'),all = F)
+View(dg_ts_inner)
+
+# Full Join
+
+dg_ts_full<-merge(dg,ts,by.x = c('CustName'),by.y = c('CustomerName'),all=T)
+View(dg_ts_full)
+
+# Left Join
+
+dg_ts_Left<-merge(dg,ts,by.x=c('CustName'),by.y=c('CustomerName'),all.x=T)
+View(dg_ts_Left)
+
+# Right JOin
+
+dg_ts_right<-merge(dg,ts,by.x = c('CustName'),by.y = c('CustomerName'),all.y=T)
+View(dg_ts_right)
+
+# Left Outer Join
+ds_ts_outer_left<-merge(dg,ts,by.x = c('CustName'),by.y = c('CustomerName'),all.y = F)
+View(ds_ts_outer_left)
+
+# RESHAPING THE DATASETS
+
+store_sales<-read.table(choose.files(),sep = ',',header = T)
+View(store_sales)
+
+v1<-names(store_sales)[3:13] # Vector of monthly sales column names
+View(v1)
+
+# reshaping wide to long
+store_sales.Wide_Long1<-reshape(store_sales,idvar = c('StoreID','City'),varying = v1 ,timevar = c('Month'),
+                                v.names=c('Sales'),times=c("Jan_Sales","Feb_sales","Mar_sales","Apr_sales",
+                                                           "May_Sales","June_sales","Jul_sales","Aug_sales",
+                                                           "Sep_Sales","Oct_Sales","Nov_Sales" ),direction = 'long')
+
+View(store_sales.Wide_Long1)
+
+
+# reshaping long to wide
+
+store_sales.long_wide<-reshape(store_sales.Wide_Long1,idvar = c('StoreID','City'),v.names = c('Sales'),
+                               timevar = c('Month'),direction='wide')
+
+View(store_sales.long_wide)
+
+
+# SAMPLING
+
+# sampling
+View(stores)
+sample(stores$StoreCode,size=10)
+
+
+# stratified sampling
+
+install.packages('sampling')
+require(sampling)
+strata(stores, stratanames=NULL ,size, method=c("srswor","srswr","poisson","systematic"),pik,description=F)
+
+p=0.1
+d=stores
+stratum='Location'
+
+Text=paste0('stores$',stratum)
+size<-ceiling(table(eval(parse(text=Text))) *p)
+strat<-strata(d,stratanames = stratum,size = size,method = 'srswor')
+View(strat)
+
+dsample<-getdata(d,strat)
+View(dsample)
+table(dsample$Location)
+
+# RENAMING VARIABLES IN A DATAFRAME
+
+df2<-stores
+names(df2)
+names(df2)[names(df2)=='StoreCode'] <- 'StoreID'
+names(df2)
+
+install.packages('reshape')
+require(reshape)
+mydata<-rename(stores, c(Store='StoreCode',storeLoc='Location'))
+View(mydata)
+
+# RE-ORDERING COLUMNS
+
+stores1<-stores[c('StoreName','StoreType','Location','OperatingCost','Total_Customers','AcqCostPercust',
+                  'BasketSize','ProfitPercust','OwnStore')]
+View(stores1)
+
+stores1<-stores[c(5,4,2,1,3,6:10)]
+View(stores1)
+
+# APPENDING DATA
+
+?table()
+?prop.table()
+
+s1<-table(stores$StoreType)
+View(s1)
+s2<-prop.table(s1)
+View(s2)
+
+rbind(s1,s2)
+cbind(s1,s2)
+
+
+# FORMATING DATA
+
+# example:1
+
+Sys.time()
+format(Sys.time(),format = '%b_%d_%y_%H_%M')
+
+#example:2
+format(1:10)
+format(1:10,trim=T)
+
+
+# example:3
+
+zz<-data.frame('(row names)' = c('aaaaa','b'),check.names = F)
+format(zz)
+format(zz,justify = 'left')
+
+# example:4
+
+format(13.7)
+format(13.7, nsmall = 3)
+format(c(6,13.12,123.234),digits = 5,trim = F)
+format(c(6.0,13.12,123.234),digits = 2,nsmall = 1)
+
+format(2^31-1)
+format(2^31-1,scientific = T)
