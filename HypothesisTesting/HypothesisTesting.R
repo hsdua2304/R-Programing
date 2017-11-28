07677d7e9119e9f85bdf8e636e090816ecdac3d4
hsdua2304 <hsdua2304@hotmail.com>
2017-11-28 21:44:59 +0530

    R Programming

---
+++ HypothesisTesting/HypothesisTesting.R
@@ -0,0 +1,6 @@
+require(rmarkdown)
+require(markdown)
+require(knitr)
+
+render('E:/R-Programing/HypothesisTesting/HypothesisTesting.Rmd',html_document())
+
---
+++ TelecomAnalysis-Churn/TelecomAnalysis.R
@@ -0,0 +1,6 @@
+require(rmarkdown)
+require(markdown)
+require(knitr)
+
+render("E:/R-Programing/TelecomAnalysis-Churn/TelecomAnalysis-Churn.rmd",html_document())
+
